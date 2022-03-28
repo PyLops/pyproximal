aa07523c286bb24786b941bdfd8872beacf6128f
Marcus Valtonen Örnhag <marcusvaltonen@gmail.com>
2022-03-28 14:59:41 +0200

    Vectorize and improve documentation

--- pyproximal/proximal/Log.py
+++ pyproximal/proximal/Log.py
@@ -7,6 +7,14 @@ from pyproximal import ProxOperator
 class Log(ProxOperator):
     r"""Logarithmic penalty.
 
+    The logarithmic penalty (Log) is defined as
+
+    .. math::
+
+        Log_{\sigma,\gamma}(\mathbf{x}) = \sum_i \frac{\sigma}{\log(\gamma + 1)}\log(\gamma|x_i| + 1)
+
+    where :math:`{\sigma>0}`, :math:`{\gamma>0}`.
+
     Parameters
     ----------
     sigma : :obj:`float`
@@ -18,16 +26,39 @@ class Log(ProxOperator):
     -----
     The logarithmic penalty is an extension of the elastic net family of penalties to non-convex members, which
     should produce sparser solutions compared to the l1-penalty [1]_. The pyproximal implementation considers a scaled
-    version where
-
-    .. math::
-
-        Log_{\sigma,\gamma}(\mathbf{x}) = \sum_i \frac{\sigma}{\log(\gamma + 1)}\log(\gamma|x_i| + 1)
-
-    where :math:`{\sigma>0}`, :math:`{\gamma>0}`. This satisfies :math:`{Log_{\sigma,\gamma}(0) = 0}` and
+    version that satisfies :math:`{Log_{\sigma,\gamma}(0) = 0}` and
     :math:`{Log_{\sigma,\gamma}(1) = \sigma}`, which is suitable also for penalizing singular values. Note that when
     :math:`{\gamma\rightarrow 0}` the logarithmic penalty approaches the l1-penalty and when
     :math:`{\gamma\rightarrow\infty}` it mimicks the l0-penalty.
+
+    The proximal operator can be analyzed using the 1-dimensional case
+
+    .. math::
+        prox_{\tau Log(\cdot)}(x) = argmin_{z} Log(z) + \frac{1}{2\tau}(x - z)^2
+
+    where we assume that :math:`x\geq 0`. The minima can be obtained when :math:`z=0` or at a local minimum.
+    Consider therefore
+
+    .. math::
+        f(z) = k \log(\gamma x + 1) + \frac{1}{2} (x - z)^2
+
+    where :math:`k= \frac{\tau \sigma}{\log(\gamma + 1)}` is introduced for convenience.
+    The condition that :math:`f'(z) = 0` yields the following equation
+
+    .. math::
+        \gamma z^2 + (1-\gamma y) x + k\gamma - y = 0 .
+
+    The discriminant :math:`\Delta` is given by
+        .. math::
+        \Delta = (1-\gamma y)^2-4\gamma (k\gamma - y) .
+
+    In case the discriminant is negative the global optimum is obtained when
+    :math:`z=0`; otherwise, it is obtained when
+        .. math::
+        z = \frac{\gamma x - 1 +\sqrt{\Delta}}{2\gamma} .
+
+    Note that the other stationary point must be a local maximum since
+    :math:`\gamma>0`, and can therefore be discarded.
 
     .. [1] Friedman, J. H. "Fast sparse regression and classification",
         International Journal of Forecasting, 28(3):722 – 738, 2012.
@@ -53,14 +84,15 @@ class Log(ProxOperator):
     def prox(self, x, tau):
         k = tau * self.sigma / np.log(self.gamma + 1)
         out = np.zeros_like(x)
-        for i, y in enumerate(x):
-            b = self.gamma * np.abs(y) - 1
-            discriminant = b ** 2 - 4 * self.gamma * (k * self.gamma - np.abs(y))
-            if discriminant >= 0:
-                c = np.sqrt(discriminant)
-                r = np.array([0, (b - c) / (2 * self.gamma), (b + c) / (2 * self.gamma)])
-                val = tau * self.elementwise(r) + (r - np.abs(y)) ** 2 / 2
-                idx = np.argmin(val)
-                out[i] = r[idx]
-                out[i] *= np.sign(y)
+        b = self.gamma * np.abs(x) - 1
+        discriminant = b ** 2 - 4 * self.gamma * (k * self.gamma - np.abs(x))
+        idx = discriminant >= 0
+        c = np.sqrt(discriminant[idx])
+        # The stationary point (b[idx] - c) / (2 * self.gamma) is always a local maximum, and is therefore not checked
+        r = np.vstack((np.zeros_like(c), (b[idx] + c) / (2 * self.gamma)))
+        val = tau * self.elementwise(r) + (r - np.abs(x[idx])) ** 2 / 2
+        idx_minima = np.argmin(val, axis=0)
+        print(np.unique(idx_minima))
+        out[idx] = r[idx_minima, range(r.shape[1])]
+        out *= np.sign(x)
         return out
