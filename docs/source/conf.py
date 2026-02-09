efdfd9caf902687c9d411bf7cb0fd952b733b1cb
mrava87 <matteoravasi@gmail.com>
2026-02-09 20:22:08 +0000

    minor: fix invalid escape and make ruff behave like ruff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,25 +7,26 @@ from sphinx_gallery.sorting import ExampleTitleSortKey
 
 from pyproximal import __version__
 
-# Sphinx needs to be able to import the package to use autodoc and get the version number
-sys.path.insert(0, os.path.abspath('../../pyproximal'))
+# Sphinx needs to be able to import the package to use autodoc and
+# get the version number
+sys.path.insert(0, os.path.abspath("../../pyproximal"))
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.doctest',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.extlinks',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
-    'matplotlib.sphinxext.plot_directive',
-    'numpydoc',
-    'nbsphinx',
+    "matplotlib.sphinxext.plot_directive",
+    "numpydoc",
+    "nbsphinx",
     "sphinx_design",
     "sphinx_iconify",
-    'sphinx_gallery.gen_gallery',
-    'sphinxemoji.sphinxemoji',
+    "sphinx_gallery.gen_gallery",
+    "sphinxemoji.sphinxemoji",
 ]
 
 # intersphinx configuration
@@ -44,10 +45,10 @@ intersphinx_mapping = {
 autosummary_generate = True
 
 # Include Python objects as they appear in source files
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 
 # Default flags used by autodoc directives
-autodoc_default_flags = ['members']
+autodoc_default_flags = ["members"]
 
 numpydoc_show_class_members = False
 numpydoc_show_inherited_class_members = False
@@ -55,46 +56,49 @@ numpydoc_class_members_toctree = False
 
 sphinx_gallery_conf = {
     # path to your examples scripts
-    'examples_dirs': ['../../examples', '../../tutorials',],
+    "examples_dirs": [
+        "../../examples",
+        "../../tutorials",
+    ],
     # path where to save gallery generated examples
-    'gallery_dirs': ['gallery', 'tutorials'],
-    'filename_pattern': '\.py',
+    "gallery_dirs": ["gallery", "tutorials"],
+    "filename_pattern": r"\.py",
     # Remove the "Download all examples" button from the top level gallery
-    'download_all_examples': False,
+    "download_all_examples": False,
     # Sort gallery example by file name instead of number of lines (default)
-    'within_subsection_order': ExampleTitleSortKey,
+    "within_subsection_order": ExampleTitleSortKey,
     # directory where function granular galleries are stored
-    'backreferences_dir': 'api/generated/backreferences',
+    "backreferences_dir": "api/generated/backreferences",
     # Modules for which function level galleries are created.
-    'doc_module': 'pyproximal',
+    "doc_module": "pyproximal",
     # Insert links to documentation of objects in the examples
-    'reference_url': {'pyproximal': None},
+    "reference_url": {"pyproximal": None},
     # Allow animations
-    'matplotlib_animations': True,
+    "matplotlib_animations": True,
 }
 
 # Always show the source code that generates a plot
 plot_include_source = True
-plot_formats = ['png']
+plot_formats = ["png"]
 
 # Sphinx project configuration
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = ["_build", "**.ipynb_checkpoints", "**.ipynb", "**.md5"]
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The encoding of source files.
-source_encoding = 'utf-8-sig'
-master_doc = 'index'
+source_encoding = "utf-8-sig"
+master_doc = "index"
 
 # General information about the project
 year = datetime.date.today().year
-project = 'PyProximal'
+project = "PyProximal"
 copyright = "{}, PyLops Development Team".format(year)
 
 # Version
 version = __version__
-if len(version.split('+')) > 1 or version == 'unknown':
-    version = 'dev'
+if len(version.split("+")) > 1 or version == "unknown":
+    version = "dev"
 
 # These enable substitutions using |variable| in the rst files
 rst_epilog = """
@@ -102,12 +106,12 @@ rst_epilog = """
 """.format(year=year)
 
 html_static_path = ["_static"]
-html_last_updated_fmt = '%b %d, %Y'
-html_title = 'PyProximal'
-html_short_title = 'PyProximal'
+html_last_updated_fmt = "%b %d, %Y"
+html_title = "PyProximal"
+html_short_title = "PyProximal"
 html_favicon = "_static/favicon.ico"
 html_extra_path = []
-pygments_style = 'default'
+pygments_style = "default"
 add_function_parentheses = False
 html_show_sourcelink = False
 html_show_sphinx = True
@@ -126,20 +130,27 @@ html_css_files = [
 ]
 
 html_context = {
-    'menu_links_name': 'Repository',
-    'menu_links': [
-        ('<i class="fa fa-github fa-fw"></i> Source Code', 'https://github.com/PyLops/pyproximal'),
-        ('<i class="fa fa-users fa-fw"></i> Contributing', 'https://github.com/PyLops/pyproximal/blob/master/CONTRIBUTING.md'),
+    "menu_links_name": "Repository",
+    "menu_links": [
+        (
+            '<i class="fa fa-github fa-fw"></i> Source Code',
+            "https://github.com/PyLops/pyproximal",
+        ),
+        (
+            '<i class="fa fa-users fa-fw"></i> Contributing',
+            "https://github.com/PyLops/pyproximal/blob/master/CONTRIBUTING.md",
+        ),
     ],
     # Custom variables to enable "Improve this page"" and "Download notebook"
     # links
-    'doc_path': 'docs/source',
-    'galleries': sphinx_gallery_conf['gallery_dirs'],
-    'gallery_dir': dict(zip(sphinx_gallery_conf['gallery_dirs'],
-                            sphinx_gallery_conf['examples_dirs'])),
-    'github_project': 'PyLops',
-    'github_repo': 'pyproximal',
-    'github_version': 'master',
+    "doc_path": "docs/source",
+    "galleries": sphinx_gallery_conf["gallery_dirs"],
+    "gallery_dir": dict(
+        zip(sphinx_gallery_conf["gallery_dirs"], sphinx_gallery_conf["examples_dirs"])
+    ),
+    "github_project": "PyLops",
+    "github_repo": "pyproximal",
+    "github_version": "master",
 }
 
 # Math macros
